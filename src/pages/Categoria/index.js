d9e098373b7c3b893cd8d224779020c118105a75
EricaReis <ericareis67@outlook.com>
2023-02-12 11:47:39 -0300

    Adding categories items

--- src/pages/Categoria/index.js
+++ src/pages/Categoria/index.js
@@ -1,18 +1,27 @@
 import Header from "components/Header/index";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import styles from './Categoria.module.scss';
 
 export default function Categoria() {
     const { nomeCategoria } = useParams();
-    const categoria = useSelector(state => state.categorias.find(categoria => categoria.id === nomeCategoria));
-
+    const { categoria, itens } = useSelector(state => ({ categoria: state.categorias.find(categoria => categoria.id === nomeCategoria),
+        itens: state.itens.filter(item => item.categoria === nomeCategoria) }));
+    
     return (
         <div>
             <Header 
-                titulo={categoria.nome} 
+                titulo={categoria.nome}
                 descricao={categoria.descricao} 
                 imagem={categoria.header}
             />
+            <div className={styles.itens}>
+                {itens?.map(item => (
+                    <div key={item.id}>
+                        {item.titulo}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
